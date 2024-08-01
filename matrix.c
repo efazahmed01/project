7385623729f0755e69d101e86a5b77b76686c065
Efaz Ahmed <efazahmed.com@gmail.com>
2024-08-01 13:16:34 +0600

    matrix.c

--- matrix.c
+++ matrix.c
@@ -1,59 +1,187 @@
-<--------------intcomplete----------->
-  just add multiple between two matrix
-  -------------------------------------
+#include <stdio.h>
 
+int main() {
+    printf("What do you want?\n"
+           "1. Addition\n"
+           "2. Subtraction\n"
+           "3. Multiplication\n"
+           "4. Transpose\n");
 
-#include<stdio.h>
-int main()
-{
-    int m1,m2,m3;
-    printf("\nInput First Matrix");
+    int n;
+    printf("Select: ");
+    scanf("%d", &n);
 
-    printf("\nrow and column = ");
-    scanf("%d%d",&m1,&m2);
-    int mat1[m1][m2];
-    for(int i = 0; i < m1; i++){
-        for(int j = 0; j < m2; j++){
-            printf("mat1[%d][%d] = ",i,j);
-            scanf("%d",&mat1[i][j]);
+    printf("\n");
+    if (n == 1) { // Addition
+        int n1, n2;
+        printf("Enter Dimension:\n");
+        printf("row = ");
+        scanf("%d", &n1);
+        printf("column = ");
+        scanf("%d", &n2);
+
+        int mat1[n1][n2];
+        int mat2[n1][n2];
+
+        printf("\nMatrix 1 input:\n");
+        for (int i = 0; i < n1; i++) {
+            for (int j = 0; j < n2; j++) {
+                printf("mat1[%d][%d] = ", i + 1, j + 1);
+                scanf("%d", &mat1[i][j]);
+            }
+        }
+
+        printf("\nMatrix 2 input:\n");
+        for (int i = 0; i < n1; i++) {
+            for (int j = 0; j < n2; j++) {
+                printf("mat2[%d][%d] = ", i + 1, j + 1);
+                scanf("%d", &mat2[i][j]);
+            }
+        }
+
+        printf("\nAddition:\n");
+        int add[n1][n2];
+        for (int i = 0; i < n1; i++) {
+            for (int j = 0; j < n2; j++) {
+                add[i][j] = mat1[i][j] + mat2[i][j];
+            }
+        }
+
+        for (int i = 0; i < n1; i++) {
+            for (int j = 0; j < n2; j++) {
+                printf("%d\t", add[i][j]);
+            }
+            printf("\n");
         }
     }
+    else if (n == 2) { // Subtraction
+        int n1, n2;
+        printf("Enter Dimension:\n");
+        printf("row = ");
+        scanf("%d", &n1);
+        printf("column = ");
+        scanf("%d", &n2);
 
-    printf("\nInput Second Matrix");
+        int mat1[n1][n2];
+        int mat2[n1][n2];
 
-    printf("\nrow = %d and column = ",m2);
-    scanf("%d",&m3);
-    int mat2[m2][m3];
-    for(int i = 0; i < m2; i++){
-        for(int j = 0; j < m3; j++){
-            printf("mat2[%d][%d] = ",i,j);
-            scanf("%d",&mat2[i][j]);
+        printf("\nMatrix 1 input:\n");
+        for (int i = 0; i < n1; i++) {
+            for (int j = 0; j < n2; j++) {
+                printf("mat1[%d][%d] = ", i + 1, j + 1);
+                scanf("%d", &mat1[i][j]);
+            }
+        }
+
+        printf("\nMatrix 2 input:\n");
+        for (int i = 0; i < n1; i++) {
+            for (int j = 0; j < n2; j++) {
+                printf("mat2[%d][%d] = ", i + 1, j + 1);
+                scanf("%d", &mat2[i][j]);
+            }
+        }
+
+        printf("\nSubtraction:\n");
+        int sub[n1][n2];
+        for (int i = 0; i < n1; i++) {
+            for (int j = 0; j < n2; j++) {
+                sub[i][j] = mat1[i][j] - mat2[i][j];
+            }
+        }
+
+        for (int i = 0; i < n1; i++) {
+            for (int j = 0; j < n2; j++) {
+                printf("%d\t", sub[i][j]);
+            }
+            printf("\n");
         }
     }
+    else if (n == 3) { // Multiplication
+        int m1, m2, m3;
+        printf("\nInput First Matrix:\n");
+        printf("rows and columns = ");
+        scanf("%d%d", &m1, &m2);
+        int mat1[m1][m2];
+        for (int i = 0; i < m1; i++) {
+            for (int j = 0; j < m2; j++) {
+                printf("mat1[%d][%d] = ", i + 1, j + 1);
+                scanf("%d", &mat1[i][j]);
+            }
+        }
 
-    printf("\nOutput: \n");
-    int out[m1][m3];
+        printf("\nInput Second Matrix:\n");
+        printf("rows = %d and columns = ", m2);
+        scanf("%d", &m3);
+        int mat2[m2][m3];
+        for (int i = 0; i < m2; i++) {
+            for (int j = 0; j < m3; j++) {
+                printf("mat2[%d][%d] = ", i + 1, j + 1);
+                scanf("%d", &mat2[i][j]);
+            }
+        }
 
-    for(int i = 0; i < m1; i++){
-        for(int j = 0; j < m3; j++){
-            out[i][j] = 0;
+        printf("\nOutput:\n");
+        int out[m1][m3];
+
+        for (int i = 0; i < m1; i++) {
+            for (int j = 0; j < m3; j++) {
+                out[i][j] = 0;
+            }
+        }
+
+        // Matrix multiplication
+        for (int i = 0; i < m1; i++) {
+            for (int j = 0; j < m3; j++) {
+                for (int k = 0; k < m2; k++) {
+                    out[i][j] += mat1[i][k] * mat2[k][j];
+                }
+            }
+        }
+
+        for (int i = 0; i < m1; i++) {
+            for (int j = 0; j < m3; j++) {
+                printf("%d\t", out[i][j]);
+            }
+            printf("\n");
         }
     }
+    else if (n == 4) { // Transpose
+        int n1, n2;
+        printf("Enter Dimension:\n");
+        printf("row = ");
+        scanf("%d", &n1);
+        printf("column = ");
+        scanf("%d", &n2);
 
-    // Matrix multiplication
-    for (int i = 0; i < m1; i++) {
-        for (int j = 0; j < m3; j++) {
-            for (int k = 0; k < m2; k++) {
-                     out[i][j] += mat1[i][k] * mat2[k][j];
-           }
-       }
-    }
+        int mat[n1][n2];
+        int trans[n2][n1];
 
-    for(int i = 0; i < m1; i++){
-        for(int j = 0; j < m3; j++){
-            printf("%d ",out[i][j]);
+        printf("\nMatrix input:\n");
+        for (int i = 0; i < n1; i++) {
+            for (int j = 0; j < n2; j++) {
+                printf("mat[%d][%d] = ", i + 1, j + 1);
+                scanf("%d", &mat[i][j]);
+            }
         }
-        printf("\n");
+
+        // Compute transpose
+        for (int i = 0; i < n1; i++) {
+            for (int j = 0; j < n2; j++) {
+                trans[j][i] = mat[i][j];
+            }
+        }
+
+        printf("\nTranspose:\n");
+        for (int i = 0; i < n2; i++) {
+            for (int j = 0; j < n1; j++) {
+                printf("%d\t", trans[i][j]);
+            }
+            printf("\n");
+        }
+    }
+    else {
+        printf("Enter correct value!\n");
     }
 
+    return 0;
 }
